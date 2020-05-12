91c8a636a4e026026b65778a3adf5fe8e152d061
Wallace Cardoso Gomes <wallacecardosogomes@gmail.com>
2020-05-12 17:43:15 +0000

    chore: mocking a function

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,7 +1,15 @@
 const TodoController = require('../../controllers/todo.controller');
 
+const TodoModel = require('../../model/todo.model');
+
+TodoModel.create = jest.fn(); //não tem como saber se uma função está sendo chamada se não usar mock
+
 describe("Todo Controller.createTodo", () => {
     it("should have a createTodo function", () => {
         expect(typeof TodoController.createTodo).toBe("function");
+    });
+    it("should call TodoModel.create", () => {
+        TodoController.createTodo();
+        expect(TodoModel.create).toBeCalled();
     });
 });
