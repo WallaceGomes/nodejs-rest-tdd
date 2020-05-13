6d83d78f808cb0e12f412c3f8350a1acf1768277
Wallace Cardoso Gomes <wallacecardosogomes@gmail.com>
2020-05-13 15:25:02 +0000

    chore: integration tests

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
 const app = express();
+
+const todoRoutes = require('./routes/todo.routes');
+
+app.use('/todos', todoRoutes);
 
 app.get('/', (req, res) => {
     res.json('Hello World!');
@@ -9,3 +12,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server ruinning!');
 });
+
+module.exports = app;
