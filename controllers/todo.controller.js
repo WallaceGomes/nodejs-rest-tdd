40793d78991a3df6cee7f8e953592fd3a04b5575
unknown <wallacecardosogomes@gmail.com>
2020-05-13 23:55:07 -0300

    chore: integration test for error handling

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -5,9 +5,10 @@ exports.createTodo = async (req, res, next) => {
     let createdModel;
     try{
         createdModel = await TodoModel.create(req.body);
+        res.status(201).json(createdModel);
     }catch(err){
         //const errorMessage = { message: "Done property missing" };
+        res.status(500).json({ message: err.message });
         next(err);
     }
-    res.status(201).json(createdModel);
 };
